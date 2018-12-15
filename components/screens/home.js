3b397a195e23471076bccb88627a8fd789f0252d
James Childress <jchildress@cvent.com>
2018-12-15 01:40:53 -0800

    working tab bar

--- components/screens/home.js
+++ components/screens/home.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import { Text, View, Button } from 'react-native';
 
 export default class HomeScreen extends Component {
+
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerRight: (
+        <Button
+          onPress={() => navigation.navigate('searchResults')}
+          title="Results"
+        />
+      ),
+    };
+  };
+
   render() {
     return (
       <View>
         <Text>Home Screen</Text>
         <Button
-          title="Go to Details"
+          title="Go to Search Results"
           onPress={() => this.props.navigation.navigate('searchResults')}
         />
       </View>
--- components/screens/search-results.js
+++ components/screens/search-results.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 export default class SearchResultsScreen extends Component {
+  static navigationOptions = {
+    title: 'Search Results',
+  };
+
   render() {
     return (
       <View>
