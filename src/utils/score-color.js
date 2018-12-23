135a7c0aaff4b786777c972589b862002a8a1b4f
James Childress <jchildress@cvent.com>
2018-12-22 18:58:10 -0800

    improved color gradient

--- src/utils/score-color.js
+++ src/utils/score-color.js
@@ -6,8 +6,6 @@ const { minimumScore, maximumScore } = config;
 const gradient = new Gradient([
   [237,  68, 45],
   [239, 114, 48],
-  [249, 204, 51],
-  [170, 214, 30],
   [ 91, 173, 12],
   [ 61, 130,  5],
 ]);
@@ -15,6 +13,5 @@ const gradient = new Gradient([
 export default function scoreColor(score) {
   const normalScore = Math.max(minimumScore, Math.min(maximumScore, score));
   const percent = (normalScore - minimumScore) / (maximumScore - minimumScore);
-  console.log(normalScore, percent, gradient.calcHex(percent));
   return gradient.calcHex(percent);
 };
