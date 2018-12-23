c3cb8d2816e76789118a8cd3450802993270c312
James Childress <jchildress@cvent.com>
2018-12-22 21:44:21 -0800

    link react-native-geocoder

--- android/app/src/main/java/com/pdxfoodspyrn/MainApplication.java
+++ android/app/src/main/java/com/pdxfoodspyrn/MainApplication.java
@@ -3,6 +3,7 @@ package com.pdxfoodspyrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.devfd.RNGeocoder.RNGeocoderPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGeocoderPackage(),
             new MapsPackage(),
             new RNGestureHandlerPackage()
       );
